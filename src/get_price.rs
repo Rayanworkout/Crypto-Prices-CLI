75ba8f72ace4ec76591bea0316038c02800ce84a
rayan <rayan13170@protonmail.com>
2024-05-12 01:47:44 +0200

    added force mode

--- src/get_price.rs
+++ src/get_price.rs
@@ -1,21 +1,23 @@
-use std::collections::HashMap;
-
 use crate::utils;
+use std::collections::HashMap;
+use std::io;
 
 pub fn get_price(token: &str, token_list: &[String; 5]) {
     if !token_list.contains(&token.to_lowercase().to_owned()) {
         println!(
-            "\n> \"{}\" is not in the tokens list, please correct your input: ",
+            "\n> \"{}\" is not in the tokens list, do you want to make the API call anyway ? y/n",
             token
         );
 
-        let input_fix = utils::collect_input_arg();
+        let mut choice = String::new();
+        io::stdin()
+            .read_line(&mut choice)
+            .expect("Failed to read line");
 
-        get_price(&input_fix[0], &token_list);
-
-        return;
-        
-
+        if !["y".to_string(), "yes".to_string()].contains(&choice.trim().to_lowercase()) {
+            println!("Aborting ...");
+            return;
+        }
     }
     let url = format!(
         "https://api.coingecko.com/api/v3/simple/price?ids={}&vs_currencies=usd",
