4a32607fe52a2c3de441043e2fe5725e2acaccc0
rayan <rayan13170@protonmail.com>
2024-05-15 22:55:06 +0200

    added error handling

--- src/get_price.rs
+++ src/get_price.rs
@@ -1,26 +1,56 @@
-use std::collections::HashMap;
-
+use std::{fmt, collections::HashMap, error::Error};
 use crate::utils;
-use std::error::Error;
 
-// #[derive(Debug)]
-// struct FetchPriceError {
-//     message: String,
-// }
+#[derive(Debug)]
+pub enum GetPriceError {
+    FetchError(String),
+    JsonDeserialize(String),
+    
+}
+
+impl fmt::Display for GetPriceError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            GetPriceError::FetchError(ref err) => write!(f, "An error occured while fetching the API: {err}"),
+            GetPriceError::JsonDeserialize(ref err) => write!(f, "An error occured while parsing the json: {err}"),
+        }
+    }
+}
+
+impl Error for GetPriceError {}
 
 /// Function to get the price of a given token.
 ///
 /// It fetches the price of a given token from the CoinGecko API.
-pub async fn get_price(token: &str) -> Result<(), Box<dyn Error>> {
+pub async fn get_price(token: &str) -> Result<(), GetPriceError> {
     let url = format!(
-    "https://api.coingecko.com/api/v3/simple/price?ids={token}&vs_currencies=usd",
+        "https://api.coingecko.com/api/v3/simple/price?ids={token}&vs_currencies=usd",
     );
 
-    let resp = reqwest::get(&url).await?.text().await?;
+    let resp = reqwest::get(&url)
+    .await.map_err(|e| GetPriceError::FetchError(e.to_string()))?
+    .text().await
+    .map_err(|e| GetPriceError::FetchError(e.to_string()))?;
+    
+    if resp.contains("error") {
+        return Err(GetPriceError::FetchError(resp));
+    }
 
-    let price_object = serde_json::from_str::<HashMap<String, HashMap<String, f64>>>(&resp)?;
-
-    let price = price_object.get(token).unwrap().get("usd").unwrap();
+    let price_object = serde_json::from_str::<HashMap<String, HashMap<String, f64>>>(&resp)
+    .map_err(|e| GetPriceError::JsonDeserialize(e.to_string()))?;
+    
+    
+    // Accessing price from object like: {"token": {"usd": XXX}}
+    let price = match price_object.get(token) {
+        Some(token) => match token.get("us") {
+            Some(value) => value,
+            None => {
+                return Err(GetPriceError::JsonDeserialize(format!("could not parse the following data: {resp}")));
+            }
+        },
+        None => return Err(GetPriceError::JsonDeserialize(format!("could not parse the following data: {resp}")))
+    };
+    
 
     let formatted_price: String;
 
--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,9 @@ async fn main() {
     for token in tokens {
         if !known_tokens.contains(&token.to_lowercase().as_str()) {
             match utils::map_input_to_value(&token) {
-                Some(found_value) => get_price::get_price(&found_value).await.unwrap(),
+                Some(found_value) => get_price::get_price(&found_value).await
+                .unwrap_or_else(|err| eprintln!("{err}")),
+                
                 None => match utils::find_closest_match(&token, &known_tokens) {
                     Some(closest_name) => {
                         println!(
@@ -25,7 +27,8 @@ async fn main() {
                         //         println!("\n> Do you want to make the API call anyway ? y/n",);
 
                         match utils::confirm_choice() {
-                            true => get_price::get_price(&token).await.unwrap(),
+                            true => get_price::get_price(&token).await
+                            .unwrap_or_else(|err| eprintln!("{err}")),
                             false => println!("> Aborting API call for \"{token}\".",),
                         }
                         //     }
@@ -38,7 +41,8 @@ async fn main() {
                 },
             }
         } else {
-            get_price::get_price(&token).await.unwrap();
+            get_price::get_price(&token).await
+            .unwrap_or_else(|err| eprintln!("{err}"));
         }
     }
 }
